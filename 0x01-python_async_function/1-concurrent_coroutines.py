ffee8f18e6cda7a5606e50dbd8435f34cf4f8c36
Lapololucy <lapololucy@gmail.com>
2023-08-09 08:44:52 +0000

    execute multiple coroutines at the same time with async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,37 +1,31 @@
 #!/usr/bin/env python3
-
-
+"""Execute multiple coroutines at the same time with async
+mandatory
+"""
+import asyncio
 from typing import List
 
 
-''' Import wait_random from the previous python file that you’ve
-    written and write an async routine called wait_n that takes
-    in 2 int arguments (in this order): n and max_delay. You will
-    spawn wait_random n times with the specified max_delay.
-
-    wait_n should return the list of all the delays (float values).
-    The list of the delays should be in ascending order without using
-    sort() because of concurrency.
-'''
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    ''' returns a sorted list of float numbers gotten randomly'''
-    wait_random = __import__('0-basic_async_syntax').wait_random
+    """Spawn wait_random function n times
 
-    delay_list = []
-    i = 0
+    Args:
+        n (int): number of time wait _random should be callled.
+        max_delay (int): delay period
 
-    while i < n:
-        delay_list.append(await wait_random(max_delay))
-        i += 1
+    Returns:
+        List[float]: List of all the delays in sorted order
+    """
+    # gather with an unpacked list of awaitables
+    res = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
 
-    return sorted(delay_list)
+    # Sort result in ascending order
+    for i in range(len(res)):
+        for j in range(i+1, len(res)):
+            if (res[i] > res[j]):
+                res[i], res[j] = res[j], res[i]
 
-
-if __name__ == '__main__':
-    import asyncio
-
-    print(asyncio.run(wait_n(5, 2)))
-    print(asyncio.run(wait_n(7, 4)))
-    print(asyncio.run(wait_n(10, 0)))
+    return res
